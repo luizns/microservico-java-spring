f60d4db441d8d2a4521bdd50489d0ee756263d1e
Luiz Nascimento <luns.wm@hotmail.com>
2021-11-12 14:17:05 -0300

    Hystrix fault tolerance

--- hr-payroll/src/main/java/com/luizns/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/luizns/hrpayroll/resources/PaymentResource.java
@@ -1,13 +1,15 @@
 package com.luizns.hrpayroll.resources;
 
-import com.luizns.hrpayroll.entities.Payment;
-import com.luizns.hrpayroll.services.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.luizns.hrpayroll.entities.Payment;
+import com.luizns.hrpayroll.services.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 @RequestMapping(value = "/payments")
@@ -16,9 +18,15 @@ public class PaymentResource {
     @Autowired
     private PaymentService service;
 
-    @GetMapping(value = "/{wordId}/days/{days}")
-    public ResponseEntity<Payment> getPayment(@PathVariable Long wordId, @PathVariable Integer days) {
-        Payment payment = service.getPayment(wordId, days);
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
+    @GetMapping(value = "/{workerId}/days/{days}")
+    public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
+        Payment payment = service.getPayment(workerId, days);
+        return ResponseEntity.ok(payment);
+    }
+
+    public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days) {
+        Payment payment = new Payment("Brann", 400.0, days);
         return ResponseEntity.ok(payment);
     }
 }
--- hr-worker/src/main/java/com/luizns/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/luizns/hrworker/resources/WorkerResource.java
@@ -26,14 +26,22 @@ public class WorkerResource {
     @Autowired
     private WorkerRepository repository;
 
-    @RequestMapping
-    public ResponseEntity<List<Worker>> findAll(){
+    @GetMapping
+    public ResponseEntity<List<Worker>> findAll() {
         List<Worker> list = repository.findAll();
         return ResponseEntity.ok(list);
     }
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
+
+		/*
+		try {
+			Thread.sleep(3000L);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		*/
 
         logger.info("PORT = " + env.getProperty("local.server.port"));
 
